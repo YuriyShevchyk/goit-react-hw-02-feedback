61676707a291742e15d89f681bf4b5814c11046a
YuriyShevchyk <97519385+YuriyShevchyk@users.noreply.github.com>
2022-12-04 16:33:57 +0200

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -10,5 +10,5 @@ export const Section = ({ title, children }) => (
 
 Section.propTypes = {
   title: PropTypes.string.isRequired,
-  children: PropTypes.arrayOf(PropTypes.object),
+  children: PropTypes.element,
 };
